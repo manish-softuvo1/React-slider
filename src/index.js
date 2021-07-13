181a36ef9480f33df08f2e0834d829c11355f7a2
Softuvo-Developer <manish-softuvo>
2021-07-13 18:42:26 +0530

    changes as required

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import '../public/assets/styles.css';
 import ImageList from './components/slider';
 
 ReactDOM.render(
   <React.StrictMode>
-
     <ImageList />
-
   </React.StrictMode>,
   document.getElementById('root')
 );
